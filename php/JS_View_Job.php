8c2900eb0920396e7d3990b3c0de7546644ffc3f
Sammi17 <47144010+Sammi17@users.noreply.github.com>
2019-11-09 23:43:13 +0530

    HR and JS job views

--- php/JS_View_Job.php
+++ php/JS_View_Job.php
@@ -5,7 +5,7 @@ $connection = $db->getConnection(); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Job Deatails</title>
+	<title>Job Deatails- JS</title>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
@@ -57,6 +57,12 @@ $connection = $db->getConnection(); ?>
 						echo "<table><tr><td>Job Title : </td><td>".$row1['Title']."<br></td></tr><tr><td>Description : </td><td>".$row1['Description']."</td></tr><tr><td>Salary : </td><td>".$row1['Salary']."<br></td></tr><tr><td>No of Vacancies : </td><td>".$row1['VacancyCount']."<br></td></tr><tr><td>No of Positions : </td><td>".$row1['PositionCount']."<br></td></tr></table>";					 
 					 ?>
 				</div>
+				<div>
+					<a href="../php/hr_home.php" > 
+			<button type="button" class="button button2">Apply</button>
+		</a>
+		</div>
+			</div>
 			</div>
 		</div>
 	</div>
